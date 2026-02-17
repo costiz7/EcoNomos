248602c0ce1b3a13f03aa8a9b306f7ce5821a8ba
costiz7 <costelzanescu7@gmail.com>
2026-02-17 08:58:46 +0200

    Made the login route

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { register } from "./controllers/authController.js";
+import { register, login } from "./controllers/authController.js";
 
 const router = Router();
 
+router.post('/login', login);
 router.post('/register', register);
 
 export default router;
--- server/routes/controllers/authController.js
+++ server/routes/controllers/authController.js
@@ -36,4 +36,33 @@ const register = async (req, res) => {
     }
 }
 
-export { register };
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid credentials.' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+            { id: user.id, username: user.username },
+            process.env.JWT_SECRET,
+            { expiresIn: '7d' }
+        );
+
+        res.status(200).json({
+            token,
+            user
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'We encountered some server error!', error: error.message });
+    }
+}
+
+export { register, login };
