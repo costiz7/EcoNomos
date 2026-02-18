6246f51ae85777dcbab4e86543ce32a63fc52121
costiz7 <costelzanescu7@gmail.com>
2026-02-18 15:27:37 +0200

    Implemented category controller

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import tokenCheck from "../middleware/tokenCheck.js";
 import usersController from "./controllers/usersController.js";
+import categoriesController from "./controllers/categoriesController.js";
 
 const router = Router();
 
@@ -9,5 +10,10 @@ router.use(tokenCheck);
 //User routes
 router.get('/user', usersController.getProfile);
 router.put('/user', usersController.updateProfile);
+
+//Categories routes
+router.get('/categories', categoriesController.getAllCategories);
+router.post('/categories', categoriesController.createCategory);
+router.delete('/categories/:id', categoriesController.deleteCategory);
 
 export default router;
---
+++ server/routes/controllers/categoriesController.js
@@ -0,0 +1,76 @@
+import { Category } from "../../database/associations.js";
+import { Op } from "sequelize";
+
+const getAllCategories = async (req, res) => {
+    try {
+        const categories = await Category.findAll({
+            where: {
+                [Op.or]: [
+                    { userId: req.user.id },
+                    { userId: null }
+                ]
+            },
+            order: [
+                ['userId', 'ASC'],
+                ['name', 'ASC']
+            ]
+        });
+
+        res.json(categories);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
+
+const createCategory = async (req, res) => {
+    try {
+        const { name, type, iconFile } = req.body;
+
+        if(!name) {
+            return res.status(400).json({ message: 'You forgot about the name' });
+        }
+
+        if(type !== 'income' && type !== 'expense') {
+            return res.status(400).json({ message: 'Tip invalid! Trebuie sa fie \'income\' sau \'expense\'' });
+        }
+
+        const newCategory = await Category.create({
+            name,
+            type,
+            iconFile: iconFile || 'category1',
+            userId: req.user.id
+        });
+
+        res.status(201).json(newCategory);
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error: error.message });
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const category = await Category.findByPk(id);
+
+        if(!category) {
+            return res.status(404).json({ message: 'Category not found.' });
+        }
+
+        if(category.userId === null) {
+            return res.status(403).json({ message: 'You can\'t delete this category!' });
+        }
+
+        if(category.userId !== req.user.id) {
+            return res.status(403).json({ message: 'You are not authorized to delete this category.' });
+        }
+
+        await category.destroy();
+
+        res.json({ message: 'Category deleted' });
+    } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+}
+
+export default { getAllCategories, createCategory, deleteCategory };
