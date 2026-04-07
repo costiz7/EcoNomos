3f4e5cf83736138dc894c5d59339ec401268de76
costiz7 <costelzanescu7@gmail.com>
2026-04-07 20:20:49 +0300

    Implemented ProtectedRoute for protection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Home from "./Home/Home.jsx"
+import ProtectedRoute from "./ProtectedRoute.jsx"
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ < Home/> } />
+          <Route element={ < Home/> } />
+
+          <Route element={ <ProtectedRoute/> }>
+          
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
