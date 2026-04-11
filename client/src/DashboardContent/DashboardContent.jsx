d9ab957dc363c248b6e797683ad8ddc95acae13a
costiz7 <costelzanescu7@gmail.com>
2026-04-11 19:37:21 +0300

    Preparing Dashboard page

--- client/src/DashboardContent/DashboardContent.jsx
+++ client/src/DashboardContent/DashboardContent.jsx
@@ -1,6 +1,20 @@
+import './DashboardContent.css';
+
 function DashboardContent() {
     return (
         <div className="dashboard-content-wrapper">
+            <div className="welcome-message">
+                <h1>Hello, user!</h1>
+            </div>
+            <div className="dashboard-content-upper-section">
+                <div id="dashboard-upper-firstcard" className="dashboard-upper-card"></div>
+                <div id="dashboard-upper-secondcard" className="dashboard-upper-card"></div>
+                <div id="dashboard-upper-thirdcard" className="dashboard-upper-card"></div>
+            </div>
+            <div className="dashboard-content-lower-section">
+                <div id="dashboard-lower-firstcard" className="dashboard-lower-card"></div>
+                <div id="dashboard-lower-secondcard" className="dashboard-lower-card"></div>
+            </div>
         </div>
     );
 }
